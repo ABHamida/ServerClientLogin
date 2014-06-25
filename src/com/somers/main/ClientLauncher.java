2ef019491879f1e3f9460526bf4e33e419a8e3b3
Christopher <christopher.somers1995@gmail.com>
2014-06-25 16:38:25 +0100

    Added Server and Client Object

--- src/com/somers/main/ClientLauncher.java
+++ src/com/somers/main/ClientLauncher.java
@@ -1,8 +1,10 @@
 package com.somers.main;
 
+import com.matt.client.Client;
+
 public class ClientLauncher {
 	
 	public static void main(String args[]){
-		
+		new Client(); 
 	}
 }
--- src/com/somers/main/ServerLauncher.java
+++ src/com/somers/main/ServerLauncher.java
@@ -1,10 +1,12 @@
 package com.somers.main;
 
+import com.abdul.server.*;
+
 public class ServerLauncher {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Server(); 
 	}
 
 }
